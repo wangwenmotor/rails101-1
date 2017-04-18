fdfe83207ea345601808bafbe51698a76b0be8bd
wangwenmotor <wangwenmotor@gmail.com>
2017-04-19 07:17:04 +0800

    create groups index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,14 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = "早安！你好！"
+  end
+
+  def index
+    flash[:alert] = "晚安！该睡了！"
+  end
+
+  def index
+    flash[:warning] = "这是 warning 信息！"
+  end
+  
 end
